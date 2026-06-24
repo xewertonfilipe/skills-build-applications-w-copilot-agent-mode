b4dfe16936d012523ed992a9a2b9b25eac47e934
Ewerton Filipe <xewertonfilipe@gmail.com>
2026-06-24 20:25:44 +0000

    refactor: remove StrictMode wrapper from main rendering

--- octofit-tracker/frontend/src/main.jsx
+++ octofit-tracker/frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -6,9 +5,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 )
